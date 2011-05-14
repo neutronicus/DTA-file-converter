c913b3eb0c5743d8e34a9bb83c15fbdacbb0bc3b
Adam Higuera <ahig321@gmail.com>
2011-05-14 14:18:07 -0400

    Added support for options to import_dta command

--- bin/import_dta.m
+++ bin/import_dta.m
@@ -1,10 +1,14 @@
-function [a b m] = import_dta(filename)
+function [a b m] = import_dta(filename, options)
+
+if nargin < 3
+  options = logical([1 1 1 1])
+end
 
 if nargout == 0
   import_dta_private(filename, {'a' 'b' 'm'}, ...
-                     logical([1 1 1 1]), 'base');
+                     options, 'base');
 else
-  import_dta_private (filename, {'tmp1' 'tmp2' 'tmp3'}, logical([1 1 1 1]), 'caller');
+  import_dta_private (filename, {'tmp1' 'tmp2' 'tmp3'}, options, 'caller');
   a = tmp1; b = tmp2; m = tmp3;
 end
 
